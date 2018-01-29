2774a88ff61405320ba4856c835c72f331b472ca
HinanawiTenshi <dr.paper@live.com>
2018-01-29 15:43:54 +0800

    Add logger

---
+++ logger/init.go
@@ -0,0 +1,32 @@
+package logger
+
+import (
+	"log"
+	"os"
+)
+
+const IPath string = "./logger/logs/info"
+const EPath string = "./logger/logs/error"
+
+var I *log.Logger
+var E *log.Logger
+
+func init() {
+	infoFile, err := os.OpenFile(IPath, os.O_CREATE|os.O_RDWR, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	I = log.New(infoFile, "[INFO]", log.Ldate|log.Ltime)
+
+	errorFile, err := os.OpenFile(EPath, os.O_CREATE|os.O_RDWR, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	E = log.New(errorFile, "[ERROR]", log.Ldate|log.Ltime)
+}
+
+func LogIfError(err error) {
+	if err != nil {
+		E.Println(err)
+	}
+}
