cfc0c4b303731cfdf564748c7892d7b23a7655aa
HinanawiTenshi <dr.paper@live.com>
2018-04-14 16:09:45 +0800

    Update sample_data

---
+++ sample_data/load.sql
@@ -0,0 +1,17 @@
+-- load data infile 'activities.csv' into table activities 
+-- fields terminated by ','  optionally 
+-- enclosed by '"' escaped by '"' 
+-- lines terminated by '\r\n' 
+-- ignore 1 lines;
+
+-- load data infile 'stages.csv' into table stages 
+-- fields terminated by ','  optionally 
+-- enclosed by '"' escaped by '"' 
+-- lines terminated by '\r\n' 
+-- ignore 1 lines;
+
+load data infile 'organizations.csv' into table organizations 
+fields terminated by ','  optionally 
+enclosed by '"' escaped by '"' 
+lines terminated by '\r\n' 
+ignore 1 lines;
