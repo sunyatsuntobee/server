8ab04d5983e1ddbf1d2eb1eb4f3c48ed0a48ccbc
c153197984 <c153197984@users.noreply.github.com>
2018-05-01 15:53:28 +0800

    Update users_follow_activities.go

--- models/users_follow_activities.go
+++ models/users_follow_activities.go
@@ -1,12 +1,15 @@
 package models
 
-import "time"
+import (
+	"time"
+	"github.com/sunyatsuntobee/server/logger"
+)
 
 // UsersFollowActivities Model
 type UsersFollowActivities struct {
-	ID         int       `xorm:"id INT PK NOTNULL UNIQUE AUTOINCR"`
-	UserID     int       `xorm:"user_id INT NOTNULL INDEX(user_id_idx)"`
-	ActivityID int       `xorm:"activity_id INT NOTNULL INDEX(activity_id_idx)"`
+	ID         int       `xorm:"id INT PK NOTNULL UNIQUE AUTOINCR" json:"id"`
+	UserID     int       `xorm:"user_id INT NOTNULL INDEX(user_id_idx)" json:"user_id"`
+	ActivityID int       `xorm:"activity_id INT NOTNULL INDEX(activity_id_idx)" json:"activity_id"`
 	Timestamp  time.Time `xorm:"timestamp DATETIME NOTNULL" json:"timestamp"`
 }
 
@@ -17,4 +20,28 @@ func NewUsersFollowActivities(userID int, activityID int) *UsersFollowActivities
 		ActivityID: activityID,
 		Timestamp:  time.Now(),
 	}
+}
+
+func (*UsersFollowActivitiesDataAccessObject) TableName() string {
+	return "user_follow_activities"
+}
+
+type UsersFollowActivitiesDataAccessObject struct{}
+
+var UsersFollowActivitiesDAO *UsersFollowActivitiesDataAccessObject
+
+
+func (*UsersFollowActivitiesDataAccessObject) InsertOne(
+	r *UsersFollowActivities) {
+	_, err := orm.Table(UsersFollowActivitiesDAO.TableName()).
+		InsertOne(r)
+	logger.LogIfError(err)
+}
+
+//DeleteByID delete a user-activity relationship by its ID
+func (*UsersFollowActivitiesDataAccessObject) DeleteByID(id int) {
+	var usersFollowActivities UsersFollowActivities
+	_, err := orm.Table(UsersFollowActivitiesDAO.TableName()).
+		ID(id).Delete(&usersFollowActivities)
+	logger.LogIfError(err)
 }
