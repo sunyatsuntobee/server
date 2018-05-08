35de5476daea1e068739e57648583e19de7587e4
153197984@qq.com <c153197984@users.noreply.github.com>
2018-05-08 20:59:27 +0800

    1

--- server/static/js/login.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $("button[type='submit']").click(function() {
-    window.location.href = "/photos"
-  });
-
-  $("button[type='button']").click(function() {
-    window.location.href = "/register"
-  })
-});
