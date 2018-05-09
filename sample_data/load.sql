4860731d846e3e1327607846022e249cf376cda8
ShaoBo Yi <c153197984@users.noreply.github.com>
2018-05-09 21:53:27 +0800

    Update load.sql

--- sample_data/load.sql
+++ sample_data/load.sql
@@ -1,18 +1,18 @@
-load data local infile 'E:\\organizations.csv' into table organizations 
+load data local infile '~/go/src/github.com/sunyatsuntobee/server/sample_data/organizations.csv' into table organizations 
 fields terminated by ','  optionally 
 enclosed by '"' escaped by '"' 
 lines terminated by '\r\n'
 ignore 1 lines;
 
 
-load data local infile 'E:\\activities.csv' into table activities 
+load data local infile '~/go/src/github.com/sunyatsuntobee/server/sample_data/activities.csv' into table activities 
 fields terminated by ',' 
 enclosed by '"' escaped by '"' 
 lines terminated by '\r\n' 
 ignore 1 lines;
 
 
-load data local infile 'E:\\stages.csv' into table activity_stages 
+load data local infile '~/go/src/github.com/sunyatsuntobee/server/sample_data/stages.csv' into table activity_stages 
 fields terminated by ','  optionally 
 enclosed by '"' escaped by '"' 
 lines terminated by '\r\n' 
