bf93c1ad3054495ad47b1e53a9b73f735f7d52ba
153197984@qq.com <c153197984@users.noreply.github.com>
2018-05-09 17:18:12 +0800

    数据库更行

--- sample_data/load.sql
+++ sample_data/load.sql
@@ -1,14 +1,18 @@
-load data local infile 'organizations.csv' into table organizations 
+load data local infile 'E:\\organizations.csv' into table organizations 
 fields terminated by ','  optionally 
+enclosed by '"' escaped by '"' 
+lines terminated by '\r\n'
+ignore 1 lines;
+
+
+load data local infile 'E:\\activities.csv' into table activities 
+fields terminated by ',' 
 enclosed by '"' escaped by '"' 
 lines terminated by '\r\n' 
 ignore 1 lines;
 
-load data local infile 'activities.csv' into table activities 
-fields terminated by ',' 
-ignore 1 lines;
 
-load data local infile 'stages.csv' into table activity_stages 
+load data local infile 'E:\\stages.csv' into table activity_stages 
 fields terminated by ','  optionally 
 enclosed by '"' escaped by '"' 
 lines terminated by '\r\n' 
