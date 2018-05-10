f2af1ed5c69c7f8b99d2bfec18eae8ba198167f7
ShaoBo Yi <c153197984@users.noreply.github.com>
2018-05-11 05:51:08 +0800

    Update load.sql

--- sample_data/load.sql
+++ sample_data/load.sql
@@ -1,19 +1,16 @@
 load data local infile '~/go/src/github.com/sunyatsuntobee/server/sample_data/organizations.csv' into table organizations 
-CHARACTER SET utf8 
 fields terminated by ','  optionally 
 enclosed by '"' escaped by '"' 
 ignore 1 lines;
 
 
 load data local infile '~/go/src/github.com/sunyatsuntobee/server/sample_data/activities.csv' into table activities 
-CHARACTER SET utf8 
 fields terminated by ',' 
 enclosed by '"' escaped by '"' 
 ignore 1 lines;
 
 
 load data local infile '~/go/src/github.com/sunyatsuntobee/server/sample_data/stages.csv' into table activity_stages 
-CHARACTER SET utf8 
 fields terminated by ','  optionally 
 enclosed by '"' escaped by '"' 
 ignore 1 lines;
