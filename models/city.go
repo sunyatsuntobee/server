e9e68675270db60fc2b7cf94ed395a795595fc2a
153197984@qq.com <c153197984@users.noreply.github.com>
2018-05-12 23:32:50 +0800

    del

--- models/city.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// City Model
-type City struct {
-	ID         int    `xorm:"id INT PK NOTNULL UNIQUE AUTOINCR" json:"id"`
-	Name       string `xorm:"name VARCHAR(20) NOTNULL" json:"name"`
-	ProvinceID int    `xorm:"province_id INT NOTNULL INDEX(fk_cities_province_id_idx)" json:"province_id"`
-}
--- static/js/register.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $("button[type='submit']").click(function() {
-    window.location.href = "/login"
-  });
-
-  $("button[type='button']").click(function() {
-    window.location.href = "/login"
-  })
-});
